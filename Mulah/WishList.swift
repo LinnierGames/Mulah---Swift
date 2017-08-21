d2d49094275684f0b0624cb71bbadbdde40dcfd2
Erick Sanchez <esericksanc@gmail.com>
2017-08-21 14:19:11 -0700

    Init wish lists

---
+++ Mulah/WishList.swift
@@ -0,0 +1,20 @@
+//
+//  WishList.swift
+//  Mulah
+//
+//  Created by Erick Sanchez on 8/21/17.
+//  Copyright © 2017 Erick Sanchez. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension WishList {
+    convenience init(title: String = "Untitled", dateCreated date: Date = Date(), physicalAccount account: Account, `in` context: NSManagedObjectContext) {
+        self.init(context: context)
+        
+        self.title = title
+        self.dateCreated = date as NSDate
+        self.physicalAccount = account
+    }
+}
